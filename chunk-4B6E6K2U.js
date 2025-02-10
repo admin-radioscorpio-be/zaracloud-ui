1b47f324d25c5d38814a8cd443ce644421d8d6f7
bertcarels <52321116+bertcarels@users.noreply.github.com>
2025-02-10 22:26:28 +0100

    Auto-generated commit

--- chunk-4B6E6K2U.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-Q7L6LLAK.js";var t=[{path:"",loadComponent:()=>import("./chunk-RI4V6IY6.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
--- chunk-6HRO5KLW.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-Q7L6LLAK.js";var o=[{path:"",loadComponent:()=>import("./chunk-SQR4M2XO.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
