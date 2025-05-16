d990031679db470000079d45f10e2099b22c33c3
bertcarels <52321116+bertcarels@users.noreply.github.com>
2025-05-16 15:53:04 +0200

    Auto-generated commit

---
+++ chunk-GK3T5PC7.js
@@ -0,0 +1,1 @@
+import"./chunk-GAL4ENT6.js";var o=[{path:"",loadComponent:()=>import("./chunk-Y7C6VQ2X.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
---
+++ chunk-P6GZMPHC.js
@@ -0,0 +1,1 @@
+import"./chunk-GAL4ENT6.js";var t=[{path:"",loadComponent:()=>import("./chunk-2OLLP777.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
